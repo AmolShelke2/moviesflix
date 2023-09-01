b1053b80bfb5d8cbcf4d8c6e118cbf78c7f5a00e
Amol <shelkeamol897@gmail.com>
2023-09-01 13:03:33 +0530

    [feat]: styled header component and added event handlers

---
+++ assets/js/global.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// Add events for
+// multiple elements
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (const elem of elements) elem.addEventListener(eventType, callback);
+};
+
+/**
+ * Toggle search box in mobile device || small screen
+
+ */
+
+const searchBox = document.querySelector("[search-box]");
+const searchTogglers = document.querySelectorAll("[search-toggler]");
+
+addEventOnElements(searchTogglers, "click", function () {
+  searchBox.classList.toggle("active");
+});
